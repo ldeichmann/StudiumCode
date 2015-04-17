48b25c737b1774621e2ef7a12d90ca71a62fafff
Jan Lukas Deichmann <github@pezam.de>
2015-04-17 10:07:01 +0200

    move to mac address based scanning, fuck technicolor dhcp

--- lukas/Scripts/hibernator.py
+++ lukas/Scripts/hibernator.py
@@ -2,22 +2,25 @@
 
 ## config
 
-machines = ['192.168.0.10', '192.168.0.13'] #shepard, chromecast
-
+#machines = ['192.168.0.10', '192.168.0.13'] #shepard, chromecast
+machines = ['12:34:56:78:90:ab']
 ##
-import os
+import os, nmap
 
 def ping(host):
 	return os.system("ping -c 1 " + host)
 
 def main():
 	up = False
-	for host in machines:
-		response = ping(host)
-		print("host: ", host, "response", response)
-		if response == 0:
-			up = True
-			break
+	nm = nmap.PortScanner()
+	nm.scan('192.168.0.10-20', arguments='-O')
+	for h in nm.all_hosts():
+		if 'mac' in nm[h]['addresses']:
+#			print(nm[h]['addresses']['mac'], nm[h]['vendor'])
+			if nm[h]['addresses']['mac'] in machines:
+#				print(nm[h]['addresses']['mac'], "is up")
+				up = True
+				break
 
 	if up == False:
 		hibernate()
