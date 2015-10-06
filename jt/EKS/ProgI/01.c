0114723b0fd13bcb8df3fd6cc645e354e0a1c56a
Tjorve Sob <jt@sobieski.de>
2015-10-06 16:11:07 +0200

    first prog prak

---
+++ jt/EKS/ProgI/01.c
@@ -0,0 +1,12 @@
+// Aufgabe 2: 20 Punkte
+// Entwickeln Sie ein C-Programm, das die ersten drei Zeilen aus “Brave New World”
+// (Schöne Neue Welt) auf der Konsole ausgibt.
+
+#include <stdio.h>
+
+int main(void) {
+
+
+
+	return 0;
+}
