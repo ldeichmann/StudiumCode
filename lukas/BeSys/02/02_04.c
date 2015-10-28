e2ccc9ddae68620f9a1b9a9eae766e2ef3c2781c
Jan Lukas Deichmann <github@pezam.de>
2015-10-28 08:27:46 +0100

    remove break from 04

--- lukas/BeSys/02/02_04.c
+++ lukas/BeSys/02/02_04.c
@@ -10,35 +10,40 @@ int main() {
     int status;
 
     for( i = 0; i < 4; i++ ) { // Fork 1, four times
-        pid = fork();
-        if ( pid == 0 ) {
-            printf("F1: Kind: PID: %d; PPID: %d\n", getpid(), getppid() );
-            for( j = 0; j < 2; j++ ) {  // Fork 2, twice
-                pid = fork();
-                if (pid == 0) {
-                    sleep(1);
-                    printf("F2: Kind: PID: %d; PPID: %d\n", getpid(), getppid() );
-                    pid = fork();
-                    if (pid == 0) {  // Fork 3, one time
-                        sleep(8);
-                        printf("F3: Kind: PID: %d; PPID: %d\n", getpid(), getppid() );
-                        sleep(5);
-                        break;
-                    }
-                    sleep(2);
-                    break;
-                }
-            }
-            sleep(2);
-            break;
-        }
+        if (getpid() == ppid) {
+	        pid = fork();
+	        if ( pid == 0 ) {
+	            printf("F1: Kind: PID: %d; PPID: %d\n", getpid(), getppid() );
+	            for( j = 0; j < 2; j++ ) {  // Fork 2, twice
+	                if (getppid() == ppid) {
+		                pid = fork();
+		                if (pid == 0) {
+		                    sleep(1);
+		                    printf("F2: Kind: PID: %d; PPID: %d\n", getpid(), getppid() );
+		                    pid = fork();
+		                    if (pid == 0) {  // Fork 3, one time
+		                        sleep(8);
+		                        printf("F3: Kind: PID: %d; PPID: %d\n", getpid(), getppid() );
+		                        sleep(5);
+		                    }
+		                    sleep(2);
+		                }
+		            }
+	            }
+	            sleep(2);
+	        }
+	    }
     }
     if (ppid == getpid()) {
         printf("I am the parent, my PID is %d and I'm waiting now.\n", getpid());
         wait(&status);
+        wait(&status);
+        wait(&status);
+        wait(&status);
         printf("I am the parent, we're done here.\n");
     } else {
         printf("I am %d and I am waiting.\n", getpid());
+        wait(&status);
         wait(&status);
         printf("I am %d and I am done.\n", getpid());
     }
