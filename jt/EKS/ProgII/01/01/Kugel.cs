edd6467151d55d1b6db2872d71eee08a3ca8451d
tweekx <theonlytweekx@gmail.com>
2016-03-24 11:52:44 +0100

    first commit ProgII ex 01

---
+++ jt/EKS/ProgII/01/01/Kugel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01
+{
+   class Kugel {
+	
+	private double _kugelRadius;
+	
+	public Kugel() {
+		
+	}
+
+	public Kugel(double kRadius) {
+		if(kRadius > 0)
+			KugelRadius = kRadius;
+		else
+			System.Console.WriteLine("Radius muss größer als 0 sein");
+	}
+
+    public double KugelRadius { get; set; }
+
+	public double BerechneVolumen() {
+		
+		return 4/3*Math.PI*KugelRadius*KugelRadius*KugelRadius;
+	}
+
+	public double BerechneOberflaeche() {
+		
+		return 4*Math.PI*KugelRadius*KugelRadius;
+	}
+
+	public void Aufblasen() {
+        KugelRadius *= 8;
+	}
+
+	
+	public bool istKleinerGleich(Kugel K) {
+		if (KugelRadius <= K.KugelRadius)
+            return true;
+        else 
+            return false;
+
+	}
+   }
+}
---
+++ jt/EKS/ProgII/01/01/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kugel kugel1 = new Kugel(17);
+            Kugel kugel2 = new Kugel(28);
+
+            if (kugel1.istKleinerGleich(kugel2) == true)
+                System.Console.WriteLine("kugel1 ist kleiner oder gleich kugel2");
+            else
+                System.Console.WriteLine("kugel2 ist kleiner oder gleich kugel1");
+
+
+            System.Console.WriteLine(kugel1.KugelRadius);
+            System.Console.WriteLine(kugel2.KugelRadius);
+
+            System.Console.WriteLine(kugel1.BerechneVolumen());
+            System.Console.WriteLine(kugel2.BerechneVolumen());
+
+            System.Console.WriteLine(kugel1.BerechneOberflaeche());
+            System.Console.WriteLine(kugel2.BerechneOberflaeche());
+
+            kugel1.Aufblasen();
+            kugel2.Aufblasen();
+
+            System.Console.WriteLine(kugel1.BerechneVolumen());
+            System.Console.WriteLine(kugel2.BerechneVolumen());
+
+            System.Console.WriteLine(kugel1.BerechneOberflaeche());
+            System.Console.WriteLine(kugel2.BerechneOberflaeche());
+
+            System.Console.ReadLine();
+        }
+    }
+}
